1ee381e5c8be6ac29027a7d167326a9df2543dbd
Muhammad Ridwan <ridhoang93@gmail.com>
2023-12-27 22:26:21 +0800

    add bb_plot for modular code

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,6 @@
+from .bb_plot import bb_plot
 from .spinner import spinner
 from .prompt import prompt
 from .menu import menu
 
-__all__ = "spinner", "prompt", "menu"
+__all__ = "spinner", "prompt", "bb_plot", "menu"
