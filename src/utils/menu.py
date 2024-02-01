f1b39d122e95d0a6688106b15c6d697e658e65d7
@i-onee <ridhoang93@gmail.com>
2024-02-02 03:50:46 +0800

    refactor menu generator and add glyph icon

--- src/utils/menu.py
+++ src/utils/menu.py
@@ -4,10 +4,11 @@ from rich.console import Console
 console = Console()
 
 def menu(title, item):
-	heading = f"{'=' * 10} {title} {'=' * 10}"
-	line = f"{'=' * len(heading)}"
+	header = f"{' ' * 10}{title}{' ' * 10}"
+	line_d, line_s = f"{'═' * len(header)}", f"{'─' * len(header)}"
 
-	console.print(f"+ {heading} +")
-	for _, (key, value) in enumerate(item.items()):
-		console.print(f"| {key}. {value.ljust(len(line) - 3)} |")
-	console.print(f"+ {line} +")
+	console.print(f"╔{line_d}╗")
+	console.print(f"║{header}║")
+	console.print(f"╟{line_s}╢")
+	for _, (key, value) in enumerate(item.items()): console.print(f"║ {key}. {value.ljust(len(header) - 5)} ║")
+	console.print(f"╚{line_d}╝\n")
