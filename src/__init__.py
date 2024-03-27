87652bb291967e975a6adf67af871ac6b61aab5f
@i-onee <ridhoang93@gmail.com>
2024-03-27 22:55:01 +0800

    refactor: add Trainer class for modular export-import

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from .missing_labels import MissingLabels
 from .annotator import Annotator
 from .predictor import Predictor
+from .trainer import Trainer
 
-__all__ = "Annotator", "MissingLabels", "Predictor"
+__all__ = "Annotator", "MissingLabels", "Predictor", "Trainer"
