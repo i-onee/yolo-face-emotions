4596b246d453de14f9ba9a8f987556f7e85dd296
i-onee <ridhoang93@gmail.com>
2024-08-09 11:05:21 +0800

    Revert "docs: add inline comments to ile_lister function"
    This reverts commit 41738d001d8e01002b816b7e231cf5635d0b2a6d.

--- src/utils/file_lister.py
+++ src/utils/file_lister.py
@@ -4,16 +4,9 @@ from os import walk
 
 
 def file_lister(target, with_path=False):
-    # initialize lists for path and file list
     p_list, f_list = [], []
-
-    # walk through the directory
     for root, _, files in walk(target):
-        # sort files by number in file name
         for name in sorted(files, key=lambda s: int(findall(r"\d+", s)[0])):
-            # add full path to p_list
             p_list.append(f"{root}/{name}")
-            # add just filename to f_list
             f_list.append(name)
-    # return tuple of list if with_path is true, otherwise return only p_list
     return (p_list, f_list) if with_path else f_list
